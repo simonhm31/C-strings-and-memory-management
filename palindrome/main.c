6618a79b4d4b2c862b8898151ab29a25b6778dfd
Simon H-M <simonhm31@gmail.com>
2024-10-22 19:00:57 -0500

    fixing leaks

--- palindrome/main.c
+++ palindrome/main.c
@@ -20,7 +20,9 @@ int main(int argc, char *argv[]) {
     // Without it, nothing will ever be a palindrome, unless it happens
     // to somehow start with a '\n'.
     line[strcspn(line, "\n")] = '\0';
+    char *result = palindrome(line);
     printf("Is the string <%s> a palindrome? %s\n", line, palindrome(line));
+    free_palindrome_result(result);
   }
 
   free(line);
--- palindrome/palindrome.c
+++ palindrome/palindrome.c
@@ -46,5 +46,10 @@ char *palindrome(char const *str) {
     answer[2] = '\0';
   }
 
+  free(rev);
   return answer;
+}
+
+void free_palindrome_result(char *result) {
+  free(result); // Free the memory allocated for the answer string
 }
