d3fed04f79383e89334b84845f29cf3e9da840a2
Simon H-M <simonhm31@gmail.com>
2024-10-23 11:09:35 -0500

    disemvowel memory leaks

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,50 @@
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 #include "disemvowel.h"
 
+// Function to determine if a character is a vowel
+int is_vowel(char c) {
+    int is_lowercase, is_uppercase;
+
+    // Check if the character is a lowercase vowel
+    is_lowercase = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    // Check if the character is an uppercase vowel
+    is_uppercase = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+
+    // Return true if the character is either a lowercase or uppercase vowel
+    return is_lowercase || is_uppercase;
+}
+
+// Function to remove vowels from a string
 char *disemvowel(char *str) {
-  return (char*) "";
+    int consonant_count = 0, length, i;
+
+    // Determine the length of the input string
+    length = strlen(str);
+
+    // Count the number of non-vowel characters (consonants)
+    for (i = 0; i < length; ++i) {
+        if (!is_vowel(str[i])) {
+            consonant_count++;
+        }
+    }
+
+    // Allocate memory for the resulting string, including space for the null terminator
+    char *result = (char*) calloc(consonant_count + 1, sizeof(char));
+    int j = 0;
+
+    // Copy non-vowel characters into the result string
+    for (i = 0; i < length; ++i) {
+        if (!is_vowel(str[i])) {
+            result[j++] = str[i];
+        }
+    }
+
+    // Null-terminate the result string
+    result[j] = '\0';
+
+    // Return the resulting string
+    return result;
 }
--- disemvowel/main.c
+++ disemvowel/main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "disemvowel.h"
 
 int main(int argc, char *argv[]) {
   char *line;
-  size_t size;
-  
-  size = 100;
-  line = (char*) malloc (size + 1);
+  size_t buffer_size = 100;
 
-  while (getline(&line, &size, stdin) > 0) {
-    printf("%s\n", disemvowel(line));
+  line = (char*) malloc(buffer_size + 1);
+
+  while (getline(&line, &buffer_size, stdin) > 0) {
+    char* result = disemvowel(line);
+    printf("%s\n", result);
+    free(result);
   }
+
+  free(line);
 }
